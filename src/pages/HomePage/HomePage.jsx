4380a8b43df072efed7394631da424950c83332a
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-08-18 23:36:10 +0100

    counter added to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,10 +4,11 @@ import s from './HomePage.module.css';
 import AuthNav from '../../components/AuthNav/AuthNav';
 import { UserMenu } from '../../components/UserMenu/UserMenu';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
-
+import { selectFilteredContacts } from '../../redux/contacts/selectors'; 
 
 const HomePage = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const contacts = useSelector(selectFilteredContacts); 
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -21,11 +22,19 @@ const HomePage = () => {
   return (
     <div className={s.wrapper}>
       <h1 className={s.title}>Be closer to your friends and loved ones!</h1>
+       {isLoggedIn ? (
+        <p className={s.description}>You've got {contacts.length} contacts in your Phonebook.</p>
+      ) : (
       <p className={s.description}>
         A simple way to gather all your important contacts...
-      </p>
+        </p>
+             )}
       {screenWidth >= 1024 ? (
-        isLoggedIn ? <UserMenu /> : <AuthNav />
+        isLoggedIn ? (
+            <UserMenu />
+        ) : (
+          <AuthNav />
+        )
       ) : null}
     </div>
   );
